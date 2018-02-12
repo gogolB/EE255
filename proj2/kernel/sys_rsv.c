6a50fe80d073306e10ed816a690d9f018d6ffe8a
Gogol <sbhat006@ucr.edu>
2018-02-11 16:17:25 -0800

    Most of the set rsv is done

--- proj2/kernel/sys_rsv.c
+++ proj2/kernel/sys_rsv.c
@@ -1,11 +1,53 @@
 #include <linux/kernel.h>
+#include <linux/time.h>
+#include <linux/sched.h>
 
-asmlinkage int set_rsv(pid_t pid, struct timespec *C, struct timespect *T)
+bool C_lessthan_T(struct timespec *C, struct timespec *T)
+{
+	if(C->tv_sec == T->tv_sec)
+		return C->tv_nsec <= T->tv_nsec;
+	else
+		return C->tv_sec <= T->tv_sec; 
+}
+
+
+asmlinkage int sys_set_rsv(pid_t pid, struct timespec *C, struct timespec *T)
+{
+	pid_t target_pid;
+
+	// Check all the params for nulls
+	if(C == 0 || T == 0)
+	{
+		// We got bad addresses for C or T
+		printk(KERN_ALERT"Recieved bad address for C(0x%p) or T(0x%p)",C, T);
+		return -1;
+	}
+
+	// Make sure that C < T or C/T is <= 1
+	if(!C_lessthan_T(C,T))
+	{
+		printk(KERN_ALERT"C is not <= T");
+		return -1;
+	}
+	
+	// Set the target PID.
+	if(pid == 0)
+	{
+		printk(KERN_INFO"Getting PID of calling task");
+		target_pid = current->pid;
+	}
+	else
+	{
+		target_pid = pid;
+	}
+
+	printk(KERN_INFO"Target PID: %u", target_pid);
+		
+	return 0;
+}
+
+asmlinkage int sys_cancel_rsv(pid_t pid)
 {
 	return -1;
 }
 
-asmlinkage int cancel_rsv(pid_t pid)
-{
-	return -1;
-}
