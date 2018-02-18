7a6bb7dac6160a237fec01c53cd5714e6cadac8a
Jesse Garcia <jgarc074@ucr.edu>
2018-02-18 12:06:47 -0800

    Test file test_period updated

--- proj2/apps/test_period/test_period.c
+++ proj2/apps/test_period/test_period.c
@@ -8,6 +8,8 @@
 
 int main(void)
 {
+	int budget = 10;    // Arbitrary 'budget value' in nsecs
+	
 	int count = 1;
 	struct timespec t_0;
 	struct timespec t_now;
@@ -15,9 +17,17 @@ int main(void)
 	clock_gettime(CLOCK_REALTIME, &t_0);
 	while(1)
 	{
-		clock_gettime(CLOCK_REALTIME, &t_now);
-		t_diff.tv_sec = t_now.tv_sec - t_0.tv_sec;
-		t_diff.tv_nsec = t_now.tv_nsec - t_0.tv_nsec;
+		while(1)
+		{
+			clock_gettime(CLOCK_REALTIME, &t_now);
+			t_diff.tv_sec = t_now.tv_sec - t_0.tv_sec;
+			t_diff.tv_nsec = t_now.tv_nsec - t_0.tv_nsec;
+			if (t_diff.tv_nsec > budget)
+			{
+				break;
+			}
+		}		
+			
 		printf("period %d: %d.%09d\n", count++, t_diff.tv_sec, t_diff.tv_nsec);
 		syscall(399);
 	}
