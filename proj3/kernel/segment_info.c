83f57a02f3b045d1b2bd771b762580b7cac44dbc
Souradeep Bhattacharya <sbhat006@ucr.edu>
2018-02-24 14:59:32 -0800

    Finished HW3, no EC, no Test

--- proj3/kernel/segment_info.c
+++ proj3/kernel/segment_info.c
@@ -1,9 +1,45 @@
 #include <linux/kernel.h>
 #include <linux/sched.h>
 #include <linux/pid.h>
+#include <linux/mm_types.h>
 
 
 asmlinkage int sys_show_segment_info(int pid)
 {
-	return -1;
+	struct task_struct *task;
+	struct pid *pid_struct;
+	pid_t target_pid;
+	
+	struct mm_struct *mmp;
+
+	// Set the target PID.
+	if(pid == 0)
+	{
+		printk(KERN_INFO"[SHOW_SEGMENT]Getting PID of calling task\n");
+		target_pid = current->pid;
+	}
+	else
+	{
+		target_pid = pid;
+	}
+	
+
+	// Now we get the TCB
+	pid_struct = find_get_pid(target_pid);
+	if(pid_struct == NULL)
+	{
+		printk(KERN_ALERT"[SHOW_SEGMENT] Failed to get a pid struct. Bad PID: %u\n",target_pid);
+		return -1;
+	}	
+	
+	task = (struct task_struct*)pid_task(pid_struct,PIDTYPE_PID);
+	
+	mmp = task->mm;
+
+	printk(KERN_INFO"[Memory segment addresses of process %u]\n",target_pid);
+	printk(KERN_INFO"%#010lx - %#010lx: code segment\n",mmp->start_code, mmp->end_code);
+	printk(KERN_INFO"%#010lx - %#010lx: data segment\n",mmp->start_data, mmp->end_data);
+	printk(KERN_INFO"%#010lx - %#010lx: heap segment\n",mmp->start_brk, mmp->brk);
+
+	return 0;
 }
--- proj3/kernel/vm_areas.c
+++ proj3/kernel/vm_areas.c
@@ -1,9 +1,59 @@
 #include <linux/kernel.h>
 #include <linux/sched.h>
 #include <linux/pid.h>
-
+#include <linux/mm_types.h>
+#include <linux/mm.h>
 
 asmlinkage int sys_show_vm_areas(int pid)
 {
-	return -1;
+	struct task_struct *task;
+	struct pid *pid_struct;
+	pid_t target_pid;
+	
+	struct mm_struct *mmp;
+	struct vm_area_struct *vmp;
+	
+	// Set the target PID.
+	if(pid == 0)
+	{
+		printk(KERN_INFO"[SHOW_VM_AREA]Getting PID of calling task\n");
+		target_pid = current->pid;
+	}
+	else
+	{
+		target_pid = pid;
+	}
+
+	// Now we get the TCB
+	pid_struct = find_get_pid(target_pid);
+	if(pid_struct == NULL)
+	{
+		printk(KERN_ALERT"[SHOW_VM_AREA] Failed to get a pid struct. Bad PID: %u\n",target_pid);
+		return -1;
+	}	
+	
+	task = (struct task_struct*)pid_task(pid_struct,PIDTYPE_PID);
+
+	printk(KERN_INFO"[Memory-maped areas of process %u]\n",target_pid);
+	// Go through all the VM_Structs
+	vmp = mmp->mmap;
+	while(1)
+	{
+		printk(KERN_INFO"%#010lx - %#010lx: %lu bytes",vmp->vm_start, vmp->vm_end, vmp->vm_end - vmp->vm_start);
+		// Check if it is locked
+		if((vmp->vm_flags & VM_LOCKED) == VM_LOCKED)
+		{
+			printk(" [L]");
+		}
+		printk("\n");
+			
+		
+		// Go to the next VMA		
+		vmp = vmp->vm_next;
+		if(vmp == NULL)
+			break;
+	
+	}
+	
+	return 0;
 }
