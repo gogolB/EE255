13b7795a5313b082b699a502a618b4af05ac63f2
Souradeep Bhattacharya <sbhat006@ucr.edu>
2018-03-10 13:05:28 -0800

    Started working on Homework 4

---
+++ proj4/kernel/sys_rsv.c
@@ -0,0 +1,174 @@
+#include <linux/kernel.h>
+#include <linux/time.h>
+#include <linux/sched.h>
+#include <linux/pid.h>
+#include <linux/ktime.h>
+#include <linux/hrtimer.h>
+#include <linux/mutex.h>
+#include <linux/spinlock.h>
+#include <linux/timer.h>
+#include <linux/init.h>
+#include <linux/jiffies.h>
+#include <linux/slab.h>
+#include "sys_rsv.h"
+
+
+#define UINT64 long long int 
+#define UINT32 long int 
+
+static inline uint64_t lldiv(uint64_t dividend, uint32_t divisor)
+{
+    uint64_t __res = dividend;
+    do_div(__res, divisor);
+    return(__res);
+}
+
+static DEFINE_MUTEX(rsv_task_mutex);
+
+typedef struct {
+	pid_t pid;
+	struct timespec T;
+} tTuple; 
+
+
+static tTuple rsv_tasks[50];
+
+static int num_rsv_tasks = 0;
+
+#define timer_c_granularity 10*1000*1000
+
+bool C_lessthan_T(struct timespec *C, struct timespec *T)
+{
+	if(C->tv_sec == T->tv_sec)
+		return C->tv_nsec < T->tv_nsec;
+	else
+		return C->tv_sec < T->tv_sec; 
+}
+
+
+asmlinkage int sys_set_rsv(pid_t pid, struct timespec *C, struct timespec *T, int cpuid)
+{
+	pid_t target_pid;
+	struct task_struct *task;
+	struct pid *pid_struct;
+
+	ktime_t ktime_T;
+	ktime_t ktime_C;
+
+	// Check all the params for nulls
+	if(C == NULL || T == NULL)
+	{
+		// We got bad addresses for C or T
+		printk(KERN_ALERT"[RSV]Recieved bad address for C(0x%p) or T(0x%p)\n",C, T);
+		return -1;
+	}
+	
+	// Check and make sure that C isn't zero
+	if(C->tv_sec == 0 && C->tv_nsec == 0)
+	{
+		printk(KERN_ALERT"[RSV]C is 0!\n");
+		return -1;
+	}
+
+	// Check and make sure that T isn't zero
+	if(T->tv_sec == 0 && T->tv_nsec == 0)
+	{
+		printk(KERN_ALERT"[RSV]T is 0!\n");
+		return -1;
+	}
+		
+	// Make sure that C < T or C/T is <= 1
+	if(!C_lessthan_T(C,T))
+	{
+		printk(KERN_ALERT"[RSV]C is not <= T\n");
+		return -1;
+	}
+
+	if()
+
+
+	// Set the target PID.
+	if(pid == 0)
+	{
+		printk(KERN_INFO"[RSV]Getting PID of calling task\n");
+		target_pid = current->pid;
+	}
+	else
+	{
+		target_pid = pid;
+	}
+
+	printk(KERN_INFO"[RSV]Target PID: %u\n", target_pid);
+	
+
+	// Now we get the TCB
+	pid_struct = find_get_pid(target_pid);
+	if(pid_struct == NULL)
+	{
+		printk(KERN_ALERT"[RSV] failed to get a pid struct. Bad PID: %u\n",target_pid);
+		return -1;
+	}	
+	
+	task = (struct task_struct*)pid_task(pid_struct,PIDTYPE_PID);
+
+	
+	if(task->rsv_task == 1)
+	{
+		// Can not reserve a task that is already reserved.
+		printk(KERN_ALERT"[RSV] This task is already reserved. PID: %u\n",target_pid);
+		return -1;
+	}		
+	
+	printk(KERN_INFO"[RSV]Setting task to be reserved\n");
+	task->rsv_task = 1;
+	task->C.tv_sec = C->tv_sec;
+	task->C.tv_nsec = C->tv_nsec;
+	task->T.tv_sec = T->tv_sec;
+	task->T.tv_nsec = T->tv_nsec;
+
+	return 0;
+}
+
+asmlinkage int sys_cancel_rsv(pid_t pid)
+{
+	pid_t target_pid;
+	struct task_struct *task;
+	struct pid *pid_struct;
+	int status;
+	struct sched_param param;
+
+	if(pid == 0)
+	{
+		target_pid = current->pid;
+	}
+	else
+	{
+		target_pid = pid;
+	}
+
+	
+	// Find the TCB
+	pid_struct = find_get_pid(target_pid);
+	if(pid_struct == NULL)
+	{
+		printk(KERN_ALERT"[RSV] Failed to find a pid struct. Bad PID:%u\n",target_pid);
+		return -1;
+	}
+	
+	task = (struct task_struct *)pid_task(pid_struct, PIDTYPE_PID);
+
+	// Check if it is reserved.
+	if(task->rsv_task == 1)
+	{
+		// Clear the flag.
+		task->rsv_task = 0;
+		printk(KERN_INFO"[RSV] Rsv canceled for PID: %u\n",target_pid);
+	}
+	else
+	{
+		// Task was not reserved, no need to do anything.
+		printk(KERN_WARNING"[RSV] No Reservation for PID: %u\n",target_pid);
+		return -1;
+	}
+}
+
